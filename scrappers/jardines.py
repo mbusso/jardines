b2e2fa39dbb0ba1b35be10358827000807c5601e
mbusso <matiassbb@gmail.com>
2018-03-18 00:08:21 -0300

    parse paginas

--- scrappers/jardines.py
+++ scrappers/jardines.py
@@ -7,10 +7,14 @@ from modules import files
 
 def main():
 	soup = request.get_content_parsed("https://guia-capital-federal.escuelasyjardines.com.ar/guia-jardines-de-infantes-en-capital-federal-belgrano.htm")
-	jardines = parse(soup)
-	files.save_as_json('jardines.json', jardines)
+	jardines = parseJardines(soup)
+	paginas = parsePaginator(soup)
+	data = {}
+	data["jardines"] = jardines
+	data["paginas"] = paginas
+	files.save_as_json('jardines.json', data)
 
-def parse(soup):
+def parseJardines(soup):
 	jardines = []
 	lista = soup.find("ul", class_="lista-indice").find_all("li")
 	for child in lista:
@@ -22,6 +26,13 @@ def parse(soup):
 			data["info"] = child.find("p").text #clean
 			jardines.append(data)
 	return jardines
+
+def parsePaginator(soup):
+	paginas = []
+	anchorPaginas = soup.find("div", class_="paginator").find_all("a")
+	for anchorPagina in anchorPaginas:
+		paginas.append(anchorPagina["href"])
+	return paginas[0: len(paginas) - 1]
 
 if __name__ == "__main__":
     main()
