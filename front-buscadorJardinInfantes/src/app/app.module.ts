2a734ec499208333ca14b6a068c47c7c535e9c65
mbusso <matiassbb@gmail.com>
2018-03-20 00:00:47 -0300

    add results page

--- front-buscadorJardinInfantes/src/app/app.module.ts
+++ front-buscadorJardinInfantes/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
 
 
+import { ResultsComponent } from './results.component';
 import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      { path: 'results', component: ResultsComponent }]
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
